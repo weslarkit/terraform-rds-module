2605a2f6b7f84d85035e730918f5ca87523ee509
Wesley Wilson <wwilson@Wesleys-MacBook-Air.local>
2020-04-21 15:43:58 -0400

    Misspelling fix

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "rds_instance_endpoint" {
   value = "postgres://adminuser:PASSWORD123123123@${aws_db_instance.default.endpoint}/${var.dbname}"
 }
-output "rds_identified" {
+output "rds_identifier" {
   value = aws_db_instance.default.identifier
 }
